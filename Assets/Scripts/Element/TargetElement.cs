079aeb39ec86db3aa231af1042893b42f26b6d0f
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-10-26 06:38:18 +0800

    Rename class

---
+++ Assets/Scripts/Element/TargetElement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Game {
+	public class TargetElement : InstantElement {
+		public HoldableElement target;
+
+		public void OnHold() {
+			if(Protagonist.instance.holding == target) {
+				Active = true;
+			}
+		}
+
+		public void UpHold() {
+			Active = false;
+		}
+	}
+}
