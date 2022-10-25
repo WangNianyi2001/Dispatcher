62d3c40e0eaee4a03e6ac15e31ee8b44797ee628
徽墨行深 <18321938171@163.com>
2022-10-25 18:47:57 +0800

    Update HoldableElementTarget.cs

---
+++ Assets/Scripts/Element/HoldableElementTarget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class HoldableElementTarget : InstantElement
+    {
+        public HoldableElement target;
+        
+        public void OnHold()
+        {
+            if (Protagonist.instance.holding == target)
+            {
+                Active = true;
+            }
+        }
+
+        public void UpHold()
+        {
+            Active = false;
+        }
+
+        public void InteractTest()
+        {
+            Debug.Log("123");
+        }
+    }
+}
