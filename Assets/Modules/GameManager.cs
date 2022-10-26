3893db46fa56e66f450b64890da4aa6f24de2a78
Nianyi Wang <Wangnianyi2001@outlook.com>
2022-10-27 01:29:14 +0800

    Import knife & stamp assets

--- Assets/Modules/GameManager.cs
+++ Assets/Modules/GameManager.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Game {
 	public class GameManager : MonoBehaviour {
+		[Serializable] public struct Elements {
+			public InstantElement door;
+			public InstantElement bed;
+			public Transform envelopeDesktopAnchor;
+		}
+		public Elements elements;
+
 		public void Log(string message) {
 			Debug.Log(message);
 		}
